6c97ccdaf4ccbda288f271e796242ac1560b784f
Shadath <shadath@bitmascot.com>
2023-03-31 16:11:16 +0600

    First Commit

---
+++ overview/readMe.md
@@ -0,0 +1,2 @@
+A server that allows you to check my personal overview.
+Requests should require a valid JWT obtained from a separate /login endpoint(make your assumptions here for partial flow, OAuth2 flow is preferred).
---
+++ overview/src/test/java/com/shadath/overview/OverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadath.overview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
